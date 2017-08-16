36078b87f55c99b03f99d1dd32f56a9ce27d8775
xieliming2017 <xieliming2006@hotmail.com>
2017-08-16 00:42:46 -0400

    checkpoint angular 5 completed

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+
+ var albumDaVinci = {
+     title: 'The Genius',
+     artist: 'Leonardo da Vinci',
+     label: 'Versatile',
+     year: '1452',
+     albumArtUrl: 'assets/images/album_covers/07.png',
+     songs: [
+         { title: 'Mona Lisa', duration: '1:01' },
+         { title: 'The Last Supper', duration: '2:01' },
+         { title: 'The Vitruvian Man', duration: '3:21'},
+         { title: 'Lady with an Ermine', duration: '4:14' },
+         { title: 'Virgin of the Rocks', duration: '5:15'}
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };

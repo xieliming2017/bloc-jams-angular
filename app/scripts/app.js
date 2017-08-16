ef5fb8625440ae0b84d27355875f23bf27e4bf2d
xieliming2017 <xieliming2006@hotmail.com>
2017-08-16 02:12:24 -0400

    checkpoint angular 5 controllers completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,28 @@
-angular.module('blocJams', []);
-(function(){
-  function config($stateProvider, $locationProvider){
-    $locationProvider
+(function() {
+     function config($stateProvider, $locationProvider) {
+       $locationProvider
         .html5Mode({
-          enabled:true,
+          enabled: true,
           requireBase: false
         });
 
-    $stateProvider
-        .state('landing', {
-          url: '/',
-          templateUrl: '/templates/landing.html'
-        })
-        .state('album', {
-          url: '/album',
-          templateUrl: '/templates/album.html'
-        })
-        .state('collection', {
-          url: '/collection',
-          templateUrl: '/templates/collection.html'
-        });
-  }
-
-  angular
-      .module('blocJams, ['ui.router']')
-      .config(config);
-})();
+        $stateProvider
+          .state('landing', {
+            url: '/',
+            controller: 'LandingCtrl as landing',
+            templateUrl: '/templates/landing.html'
+          })
+          .state('album', {
+            url: '/album',
+            templateUrl: '/templates/album.html'
+          })
+          .state('collection', {
+            url: '/collection',
+            controller: 'CollectionCtrl as collection',
+            templateUrl: '/templates/collection.html'
+          });
+      }
+     angular
+             .module('blocJams', ['ui.router'])
+             .config(config);
+  })();
